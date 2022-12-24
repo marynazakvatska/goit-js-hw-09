7a8c012974967c4aca6bb7681c11feb6c545aec0
marynazakvatska <maryna.zakvatska@gmail.com>
2022-12-25 00:07:01 +0100

    third

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -5,7 +5,7 @@ const refs = {
 }
 
 let position = 0;
-let delay = 0;
+let steps = 0;
 
 refs.form.addEventListener("submit", (event) => {
   event.preventDefault();
@@ -16,16 +16,20 @@ refs.form.addEventListener("submit", (event) => {
   console.log(delay.value, step.value, amount.value );
 
  
-  setInterval(() => {
-      if (position >= Number(amount.value) ) {
+ const intervalId = setInterval(() => {
+   if (position >= Number(amount.value)) {
+    clearInterval(intervalId);
     return;
       }
-
-    position += 1;
-
-  
-   createPromise(position, Number(delay.value) += Number(step.value) )
-   }, delay.value)
+   position += 1;
+   steps += Number(step.value);
+  createPromise(position, steps + Number(delay.value))
+  .then(({ position, delay }) => {
+    console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+  })
+  .catch(({ position, delay }) => {
+    console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+  })}, delay.value)
   
 
 });
@@ -36,29 +40,11 @@ function createPromise(position, delay) {
   return new Promise((resolve, reject) => {
     const shouldResolve = Math.random() > 0.3;
     if (shouldResolve) {
-      resolve(console.log( position, delay ))
+      resolve({position, delay})
     } else {
-      reject(console.log("error"))
+      reject({position, delay})
     }
   })
 }
 
-
-/* Напиши скрипт, який на момент сабміту форми викликає функцію createPromise(position, delay) стільки разів,
-  скільки ввели в поле amount.Під час кожного виклику передай їй номер промісу(position), що створюється, і затримку,
-    враховуючи першу затримку(delay), введену користувачем, і крок(step).
- */
-
-/* 
-Доповни код функції createPromise таким чином, щоб вона повертала один проміс, який виконується або відхиляється через delay часу. 
-Значенням промісу повинен бути об'єкт, в якому будуть властивості position і delay зі значеннями однойменних параметрів. 
-Використовуй початковий код функції для вибору того, що потрібно зробити з промісом - виконати або відхилити.
- */
-createPromise(position, delay)
-  .then(({ position, delay }) => {
-    console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
-  })
-  .catch(({ position, delay }) => {
-    console.log(`❌ Rejected promise ${position} in ${delay}ms`);
-  });
 
